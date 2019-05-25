d32bdf8f7939abfabff4b32c86f82c0907bd2e51
apurba <apurba10024@gmail.com>
2019-05-26 00:27:12 +0600

    1.3 Added Log in Activity

---
+++ app/src/main/java/com/apurba/simplechatwrtc/LoginActivity.java
@@ -0,0 +1,69 @@
+package com.apurba.simplechatwrtc;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.apurba.simplechatwrtc.util.Constants;
+
+/**
+ * Login Activity for the first time the app is opened, or when a user clicks the sign out button.
+ * Saves the username in SharedPreferences.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mUsername;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mUsername = (EditText) findViewById(R.id.login_username);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            String lastUsername = extras.getString("oldUsername", "");
+            mUsername.setText(lastUsername);
+        }
+    }
+
+    /**
+     * Takes the username from the EditText, check its validity and saves it if valid.
+     *   Then, redirects to the MainActivity.
+     * @param view Button clicked to trigger call to joinChat
+     */
+    public void joinChat(View view){
+        String username = mUsername.getText().toString();
+        if (!validUsername(username))
+            return;
+
+        SharedPreferences sp = getSharedPreferences(Constants.SHARED_PREFS,MODE_PRIVATE);
+        SharedPreferences.Editor edit = sp.edit();
+        edit.putString(Constants.USER_NAME, username);
+        edit.apply();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Optional function to specify what a username in your chat app can look like.
+     * @param username The name entered by a user.
+     * @return is username valid
+     */
+    private boolean validUsername(String username) {
+        if (username.length() == 0) {
+            mUsername.setError("Username cannot be empty.");
+            return false;
+        }
+        if (username.length() > 16) {
+            mUsername.setError("Username too long.");
+            return false;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/apurba/simplechatwrtc/MainActivity.java
+++ app/src/main/java/com/apurba/simplechatwrtc/MainActivity.java
@@ -1,15 +1,46 @@
 package com.apurba.simplechatwrtc;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.apurba.simplechatwrtc.util.Constants;
+import com.pubnub.api.Pubnub;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences mSharedPreferences;
+    private String username;
+    private String stdByChannel;
+    private Pubnub mPubNub;
+
+    private ListView mHistoryList;
+    //private HistoryAdapter mHistoryAdapter;
+    private EditText mCallNumET;
+    private TextView mUsernameTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        this.mSharedPreferences = getSharedPreferences(Constants.SHARED_PREFS, MODE_PRIVATE);
+        if (!this.mSharedPreferences.contains(Constants.USER_NAME)){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+        this.username     = this.mSharedPreferences.getString(Constants.USER_NAME, "");
+        this.stdByChannel = this.username + Constants.STDBY_SUFFIX;
+
+
     }
 
 
